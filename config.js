57997b061cbfd5682e1bbbdd1d33679f36341ea3
- 木马人 - <itjkiss@gmail.com>
2019-01-04 23:45:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -40,10 +40,10 @@ module.exports = {
     // request url
     ,io:{
         home: {
-            news_json: 'https://op.cgtn.com/data/2018/priceless-culture/feiyi_list.json'
+            news_json: 'https://url/home_data.json'
         },
         list: {
-            all_news_list: 'https://op.cgtn.com/data/2018/priceless-culture/feiyi_list.json'
+            all_news_list: 'https://url/list_data.json'
         }
     }
 }
